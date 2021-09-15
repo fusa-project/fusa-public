b9cb6b40fb3f5a543e4071ba0087e3cceef4fd38
victorvargass <victorvargassandoval93@gmail.com>
2021-09-15 20:39:18 -0300

    Main component file added

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '@style/index.css'
+import '@style/google.css'
+import '@style/map.css'
+import { AuthProvider } from "@context/auth";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
+}
