8485ae299d168b16b49d25f35820a5c44dba457b
victorvargass <victorvargassandoval93@gmail.com>
2021-09-16 10:36:52 -0300

    Map test logs

--- src/components/geopositionData.js
+++ src/components/geopositionData.js
@@ -32,27 +32,34 @@ export const Map = ( {onClick} ) => {
         var latitude = location.coords.latitude
         var longitude = location.coords.longitude
         var coords = {lat: latitude, lng: longitude}
+        console.log('coords', coords)
         map.panTo([latitude, longitude]);
         setPosition(coords)
       });
     }
+    console.log('nav', navigator)
+    console.log('map', map)
   }, [map]);
 
   return (
     <div>
-      <MapContainer
-        id="map"
-        center={position}
-        zoom={13}
-        scrollWheelZoom={true}
-        style={{ height: getHeight(windowWidth), width: "100%" }}
-        whenCreated={setMap}
-      >
-      <MapEvents onClick={onClick} setPosition={setPosition}/>
-        <TileLayer url="https://api.mapbox.com/styles/v1/mvernier/ckp75i4sw396418n6gbb4psz0/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibXZlcm5pZXIiLCJhIjoiY2twNzRxeTJzMDQycTJvbzA5N2NyN283biJ9.nMykNl6xWvMe8MV8DLH-ig" />
-        <Marker position= { position } >
-        </Marker>
-      </MapContainer>
+      { position &&
+      <div>
+        <MapContainer
+          id="map"
+          center={position}
+          zoom={13}
+          scrollWheelZoom={true}
+          style={{ height: getHeight(windowWidth), width: "100%" }}
+          whenCreated={setMap}
+        >
+        <MapEvents onClick={onClick} setPosition={setPosition}/>
+          <TileLayer url="https://api.mapbox.com/styles/v1/mvernier/ckp75i4sw396418n6gbb4psz0/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibXZlcm5pZXIiLCJhIjoiY2twNzRxeTJzMDQycTJvbzA5N2NyN283biJ9.nMykNl6xWvMe8MV8DLH-ig" />
+          <Marker position= { position } >
+          </Marker>
+        </MapContainer>
+      </div>
+      }
     </div>
   );
 };
