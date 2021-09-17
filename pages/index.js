61efbb36e10e27f524bd45c29a9c1ce6f66f57cc
victorvargass <victorvargassandoval93@gmail.com>
2021-09-16 22:37:24 -0300

    Auth user for index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Login from '../src/components/login';
+import Logout from '../src/components/logout';
+import { useAuthContext } from '@context/auth';
 
 const Home = () => {   
+    const { currentUser } = useAuthContext()
     return (
       <div className="container">
           <Head>
@@ -16,14 +20,23 @@ const Home = () => {
               <p className="description">
               Análisis de las FUentes Sonoras Ambientales
               </p>
+              {
+              currentUser ?
               <div className="grid">
+              <p className="welcome">
+                  Bienvenido {currentUser.displayName}
+              </p>
               <Link href="upload_audio">
                   <a className="card">
                   <h3>Añadir Datos  &rarr;</h3>
                   <p>Añadir sus grabaciones audios.</p>
                   </a>
               </Link>
+              <Logout/>
               </div>
+              :
+              <Login/>
+              }
           </main>
 
           <footer>
