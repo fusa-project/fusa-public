7f6e9ce4c3e609933a4b371c8474f6187db6b779
Victor Vargas <victorvargassandoval93@gmail.com>
2022-09-22 14:15:04 +0000

    Firebase config updated with fusa google account

--- src/util/firebaseConfig.js
+++ src/util/firebaseConfig.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app"
 
-var firebaseConfig = {
-    apiKey: "AIzaSyA0n4b39sPYYgWYcGbvaSYOiaYaTCoC-gk",
-    authDomain: "fusaproject-9de4e.firebaseapp.com",
-    projectId: "fusaproject-9de4e",
-    storageBucket: "fusaproject-9de4e.appspot.com",
-    messagingSenderId: "13428275643",
-    appId: "1:13428275643:web:c6fcc8ed38e0d9f2f76c9e",
-    measurementId: "G-ZB9EP0T49J"
-  }
-  
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ5qVbZU00hw6H4avCGoQIGWgRWNYgjOY",
+  authDomain: "fusa-public.firebaseapp.com",
+  projectId: "fusa-public",
+  storageBucket: "fusa-public.appspot.com",
+  messagingSenderId: "648948513250",
+  appId: "1:648948513250:web:76d07e9839f4453bb2a1bb",
+  measurementId: "G-CL32XMSDNH"
+}
+ 
 const app = initializeApp(firebaseConfig)
 
 export default app;
