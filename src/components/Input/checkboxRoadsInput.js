f4333c1699d0c0315db0c6f78a39e9fd14041aa6
Victor Vargas <victorvargassandoval93@gmail.com>
2023-11-17 18:14:20 -0300

    Checkbox for roads created

---
+++ src/components/Input/checkboxRoadsInput.js
@@ -0,0 +1,142 @@
+import styled from 'styled-components'
+import { useField, useFormikContext } from 'formik'
+import Card from '@components/card'
+import {
+  InputLabel,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Slide,
+  Button
+} from '@material-ui/core'
+import { useState, forwardRef, useEffect } from 'react'
+import { taxonomyRoadsOptions } from '@data/fusa_taxonomy'
+
+const Transition = forwardRef(function Transition (props, ref) {
+  return <Slide direction='up' ref={ref} {...props} />
+})
+
+const TypeContainer = styled.div`
+  margin-bottom: 30px;
+`
+
+const TypeLabel = styled.label`
+  color: #000;
+  display: block;
+  margin-bottom: 10px;
+  font-size: 20px;
+  text-align: left;
+`
+
+const MyCheckboxInput = styled.input`
+    border: solid 1px #b1b3b5
+    border-radius: 4px;
+    width: 50px;
+`
+const CheckboxLabel = styled.label`
+  width: 200px;
+`
+
+const TagsDialog = ({
+  field,
+  props,
+  openTags,
+  handleCloseTags,
+  handleDismissTags,
+  checkedTags
+}) => {
+  return (
+    <Dialog
+      open={openTags}
+      TransitionComponent={Transition}
+      onClose={handleDismissTags}
+      aria-describedby='alert-dialog-slide-description'
+    >
+      <DialogTitle>
+        {'Seleccione los tipos de vehículos existentes en el audio/video:'}
+      </DialogTitle>
+      <DialogContent>
+        {taxonomyRoadsOptions.map((type, i) => {
+          return (
+            <TypeContainer key={i}>
+              <TypeLabel>{type.label}</TypeLabel>
+              {type.options.map((option, j) => {
+                const checked = checkedTags.includes(option.value)
+                return (
+                  <div key={j}>
+                    <CheckboxLabel htmlFor={option.label}>
+                      {option.label}
+                    </CheckboxLabel>
+                    <MyCheckboxInput
+                      {...field}
+                      {...props}
+                      type='checkbox'
+                      value={option.value}
+                      defaultChecked={checked}
+                    />
+                  </div>
+                )
+              })}
+            </TypeContainer>
+          )
+        })}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleDismissTags}>Cancelar</Button>
+        <Button onClick={handleCloseTags}>Guardar</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+const CheckboxRoadsInput = ({ label, ...props }) => {
+  const formContext = useFormikContext()
+  const [field, meta] = useField(props)
+  const [openTags, setOpenTags] = useState(false)
+  const [checkedTags, setCheckedTags] = useState([])
+
+  useEffect(() => {
+    if (formContext.isSubmitting) {
+      setCheckedTags([])
+    }
+  }, [formContext.isSubmitting])
+
+  const handleOpenTags = () => {
+    setOpenTags(true)
+  }
+  const handleCloseTags = () => {
+    setOpenTags(false)
+    setCheckedTags(field.value)
+  }
+  const handleDismissTags = () => {
+    setOpenTags(false)
+  }
+  return (
+    <div>
+      <Card>
+        <InputLabel shrink>{label}</InputLabel>
+        <Button variant='contained' component='span' onClick={handleOpenTags}>
+          Seleccionar vehículos
+        </Button>
+      {checkedTags.length > 0 &&
+        <div>
+            <p style={{wordWrap: 'break-word'}}>Seleccionadas: {checkedTags.join(' - ')}</p>
+        </div>
+      }
+      </Card>
+
+      <TagsDialog
+        field={field}
+        props={props}
+        openTags={openTags}
+        handleCloseTags={handleCloseTags}
+        handleDismissTags={handleDismissTags}
+        checkedTags={checkedTags}
+      />
+    </div>
+  )
+}
+
+export default CheckboxRoadsInput
+
