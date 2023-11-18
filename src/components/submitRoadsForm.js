415db2414cf0d0a8f78d5d7e920bf233c5b88999
Victor Vargas <victorvargassandoval93@gmail.com>
2023-11-18 02:06:31 -0300

    Test submit audio/video to api

--- src/components/submitRoadsForm.js
+++ src/components/submitRoadsForm.js
@@ -31,7 +31,7 @@ const handleRoadsSubmit = async (data, actions) => {
     var labels = []
   }
 
-  var full_data = {
+  /*var data = {
     name: data.name,
     description: data.description,
     audio: data.audio,
@@ -44,52 +44,29 @@ const handleRoadsSubmit = async (data, actions) => {
     labels: labels,
     period: data.period
   }
-
-  var period = data.period;
-  var audio = data.audio
-  var video = data.video
+  */
+  var data = {
+    audio: data.audio,
+    video: data.video,
+    period: data.period
+  }
 
   return new Promise((resolve, reject) => {
-    const promises = [];
-
-    if (audio.data !== '') {
-      const audioPromise = client.post('/predictions/audio?period=' + period, audio);
-      promises.push(audioPromise);
-    }
-    if (video.data !== '') {
-      const videoPromise = client.post('/predictions/video?period=' + period, video);
-      promises.push(videoPromise);
-    }
-    if (promises.length === 0) {
-      resolve([]);
-      return;
-    }
-
-    Promise.all(promises)
-      .then((responses) => {
-        for (const response of responses) {
-          if (response.status !== 200) {
-            reject(responses);
-            return;
-          }
+    client
+      .post('/predictions/roads', data)
+      .then(function (res) {
+        if (res.status == 200) {
+          actions.resetForm()
+          actions.setSubmitting(false)
+          document.getElementById('audio.data').value = ''
+          document.getElementById('video.data').value = ''
         }
-
-        actions.resetForm();
-        actions.setSubmitting(false);
-
-        if (audio.data !== '') {
-          document.getElementById('audio.data').value = '';
-        }
-
-        if (video.data !== '') {
-          document.getElementById('video.data').value = '';
-        }
-        resolve(responses);
+        resolve(res)
       })
-      .catch((errors) => {
-        console.error(errors);
-        reject(errors);
-      });
-  });
+      .catch(function (error) {
+        console.log(error)
+        resolve(error)
+      })
+  })
 }
 export default handleRoadsSubmit
