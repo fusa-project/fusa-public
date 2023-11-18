2447cf1a15c976504c0113b52d901631b46e17f4
Victor Vargas <victorvargassandoval93@gmail.com>
2023-11-18 01:20:56 -0300

    Test submit audio/video to api

--- src/components/submitRoadsForm.js
+++ src/components/submitRoadsForm.js
@@ -44,6 +44,52 @@ const handleRoadsSubmit = async (data, actions) => {
     labels: labels,
     period: data.period
   }
-  
+
+  var period = data.period;
+  var audio = data.audio
+  var video = data.video
+
+  return new Promise((resolve, reject) => {
+    const promises = [];
+
+    if (audio.data !== '') {
+      const audioPromise = client.post('/predictions/audio?period=' + period, audio);
+      promises.push(audioPromise);
+    }
+    if (video.data !== '') {
+      const videoPromise = client.post('/predictions/video?period=' + period, video);
+      promises.push(videoPromise);
+    }
+    if (promises.length === 0) {
+      resolve([]);
+      return;
+    }
+
+    Promise.all(promises)
+      .then((responses) => {
+        for (const response of responses) {
+          if (response.status !== 200) {
+            reject(responses);
+            return;
+          }
+        }
+
+        actions.resetForm();
+        actions.setSubmitting(false);
+
+        if (audio.data !== '') {
+          document.getElementById('audio.data').value = '';
+        }
+
+        if (video.data !== '') {
+          document.getElementById('video.data').value = '';
+        }
+        resolve(responses);
+      })
+      .catch((errors) => {
+        console.error(errors);
+        reject(errors);
+      });
+  });
 }
 export default handleRoadsSubmit
